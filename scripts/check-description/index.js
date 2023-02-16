fee9a44d7cb5d3703d8e11cc41ba2c87b4df60db
Kalden Ficklin <kficklin@taskrabbit.com>
2023-02-15 19:19:32 -0500

    Update index.js

--- scripts/check-description/index.js
+++ scripts/check-description/index.js
@@ -4,8 +4,12 @@ const github = require('@actions/github');
 async function run() {
   try {
     const pull_request = core.getInput('pull_request');
+    const pull_request2 = core.getInput('pull_request2');
+    const pull_request3 = core.getInput('pull_request3');
 
     console.log({ pull_request });
+    console.log({ pull_request2 });
+    console.log({ pull_request3 });
 
     core.setOutput("complete", true);
   } catch (error) {
