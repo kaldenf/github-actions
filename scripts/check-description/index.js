b8485308d7df2b55f5321d947c6af28ebb0304ea
Kalden Ficklin <kficklin@taskrabbit.com>
2023-02-16 09:01:27 -0500

    Update index.js

--- scripts/check-description/index.js
+++ scripts/check-description/index.js
@@ -1,17 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const DEFAULT_TICKET_URL = 'https://taskrabbit.atlassian.net/browse/TAPP-XXXX';
+const TAPP_TICKET_URL = 'https://taskrabbit.atlassian.net/browse/TAPP-';
+const TAPP_RELEASE_URL = 'https://taskrabbit.atlassian.net/projects/TAPP/versions/';
+const FAILURE_MESSAGE = 'JIRA ticket link not found'
+
 async function run() {
   try {
-    const pull_request = core.getInput('pull_request');
-    const pull_request2 = core.getInput('pull_request2');
-    const pull_request3 = core.getInput('pull_request3');
+    const { body } = github.context.payload.pull_request
 
-    console.log({ pull_request });
-    console.log({ pull_request2 });
-    console.log({ pull_request3 });
+    if (body.includes(DEFAULT_TICKET_URL)) {
+      core.setFailed(FAILURE_MESSAGE);
+      return;
+    }
 
-    core.setOutput("complete", true);
+    if (!body.includes(TAPP_TICKET_URL) && !body.includes(TAPP_RELEASE_URL)) {
+      core.setFailed(FAILURE_MESSAGE);
+      return;
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
